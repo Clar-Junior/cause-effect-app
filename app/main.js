9bd68ac0da69acd87d00a4482f6bdac2957bc416
C.Junior <clarivaljunior@hotmail.com>
2026-02-24 07:48:09 -0300

    feat: right panel show selected card from left panel

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import people from './js/data.js';
 
-
+const body = document.getElementsByTagName('body')[0]; // Obtém o elemento body, [0]
 const leftPanel = document.getElementById('left_panel');
 const rightPanel = document.getElementById('right_panel');
 
@@ -35,10 +35,61 @@ function createPersonDetails(person) {
 leftPanel.addEventListener('click', (event) => {
     const card = event.target.closest('.person-card');
     if (card) {
+        rightPanel.style.display = 'block';
+
         const name = card.querySelector('h3').textContent;
         const person = people.find(p => p.name === name);
         const details = createPersonDetails(person);
         rightPanel.innerHTML = '';
         rightPanel.appendChild(details);
+        return
     }
-});
+    
+    // Adiciona evento de clique para fechar detalhes
+      rightPanel.style.display = 'none';
+
+      
+  });
+
+  body.addEventListener('click', (event) => {
+    // console.log(event.target);    
+    const app = event.target.id === 'app';
+    if (app){
+      rightPanel.style.display = 'none';
+      return
+    }    
+    
+  });
+
+  // se clique em person-card, destaca background e color do card
+  leftPanel.addEventListener('click', (event) => {
+    const card = event.target.closest('.person-card');
+    const cards = leftPanel.querySelectorAll(".person-card");
+    if (card) {
+      cards.forEach((c) => c.classList.remove("selected"));
+      cards.forEach(c => {
+        c.style.backgroundColor = "#bfd2bf";
+        c.style.color = "#080808";
+      });
+      console.log(cards[0].style.backgroundColor);
+      
+      // person-card é um HTML collection, assim marquei a variável selectedCard com o cards com 'selected'
+      // marquei usando a classe selected para evitar que seja necessário
+      // percorrer toda a coleção para remover o background e color do
+      // card selecionado, assim basta remover a classe selected do card selecionado
+      // e adicionar a classe selected ao card clicado, e no CSS definir o background e color
+      // para a classe selected, assim quando clicar fora de um card,
+      // basta remover a classe selected do card selecionado,
+      // sem precisar percorrer toda a coleção.
+        // muda background e color do card selecionado
+        card.classList.add("selected");
+        const selectedCard = leftPanel.querySelectorAll(".person-card.selected");
+        
+        if (selectedCard.length > 0) {
+          selectedCard.forEach((c) => {
+            c.style.backgroundColor = "#fff";
+            c.style.color = "#000";
+          });
+        }
+      }
+  });
